c0ccdc2e349247ca5fe1bd81bf850fc69788cbf0
Andrzej Dybowski <dybowski@andrzejd.pl>
2019-03-03 16:56:30 +0100

    Version 1.3 (#21)
    * feat(3): add test for existing code
    
    * feat(3): add matrix to travis file
    
    * feat(3): add matrix to travis file
    
    * feat(3): add matrix to travis file
    
    * feat(3): add env test file
    
    * feat(3): add running tests
    
    * feat(3): add test for existing code
    
    * feat(3): add matrix to travis file
    
    * feat(3): add matrix to travis file
    
    * feat(3): add matrix to travis file
    
    * feat(3): add env test file
    
    * feat(3): add running tests
    
    * feat(1): add test
    
    * feat(1): add implement getting articles
    
    * feat(1): add test table
    
    * feat(1): change row in article table
    
    * feat(1): add article handler

---
+++ database/database_test.go
@@ -0,0 +1,61 @@
+package database
+
+import (
+	"github.com/andrzejd-pl/SimpleRestBlogBackend/usage"
+	"github.com/joho/godotenv"
+	"reflect"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestGetAllCategories(t *testing.T) {
+	err := godotenv.Load("../.env")
+	usage.CheckErr(err)
+	tests := []struct {
+		name string
+		want []Category
+	}{
+		{
+			"Category",
+			[]Category{
+				{1, "TestName", "TestDescription"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetAllCategories(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetAllCategories() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGetAllArticles(t *testing.T) {
+	err := godotenv.Load("../.env")
+	usage.CheckErr(err)
+	tests := []struct {
+		name string
+		want []Article
+	}{
+		{
+			"Article",
+			[]Article{
+				{
+					1,
+					Category{1, "TestName", "TestDescription"},
+					"TestTitle",
+					"TestContent",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetAllArticles(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetAllArticles() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
